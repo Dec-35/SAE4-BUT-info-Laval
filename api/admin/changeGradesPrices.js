b4202af9598553d8db9d666a45362b5bfdb69cf3
Amel8x8lemA <ameliendenis72@gmail.com>
2024-04-03 11:26:40 +0200

    fix negative grade price

--- api/admin/changeGradesPrices.js
+++ api/admin/changeGradesPrices.js
@@ -10,6 +10,13 @@ router.post('', async (req, res) => {
     return;
   }
 
+  // Vérification des prix pour s'assurer qu'ils ne sont pas négatifs
+  if (parseFloat(ironprice) < 0 || parseFloat(goldprice) < 0 || parseFloat(diamantprice) < 0) {
+    res.status(403).json({ error: 'Le prix ne peut pas être négatif' });
+    return;
+  }
+
+  // Mise à jour du prix du grade iron
   await pool.query(
     'UPDATE grade SET price = ? WHERE name = ?',
     [ironprice, 'iron'],
@@ -22,6 +29,7 @@ router.post('', async (req, res) => {
     }
   );
 
+  // Mise à jour du prix du grade gold
   await pool.query(
     'UPDATE grade SET price = ? WHERE name = ?',
     [goldprice, 'gold'],
@@ -34,15 +42,14 @@ router.post('', async (req, res) => {
     }
   );
 
+  // Mise à jour du prix du grade diamant
   await pool.query(
     'UPDATE grade SET price = ? WHERE name = ?',
     [diamantprice, 'diamant'],
     (err) => {
       if (err) {
         console.error('Impossible de changer le prix :', err);
-        res
-          .status(500)
-          .json({error: 'Impossible de changer le prix de diamant'});
+        res.status(500).json({error: 'Impossible de changer le prix de diamant'});
         return;
       }
     }
